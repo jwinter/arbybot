09708deb38a1928c96f493fc4dae2d1a08555f81
Joe Winter <jwinter@gmail.com>
2011-06-13 22:03:44 -0400

    Godspeed smeade

--- config.example.rb
+++ config.example.rb
@@ -1,6 +1,7 @@
-WEBSITE_PEOPLE = %w[bira craig efren jwinter mpereira mwilliams pete_lacey smeade]
+WEBSITE_PEOPLE = %w[bira craig efren jwinter mpereira mwilliams pete_lacey]
 SERVER = 'irc.freenode.net'
 PORT = '6667'
 SSL = true
 PASSWORD = 'foobar'
-
+GITHUB_USER = 'someguy'
+GITHUB_PASS = 'same as the combination to his luggage'
